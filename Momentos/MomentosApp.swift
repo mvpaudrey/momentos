3684868a7e0d1f850c82d15b7a27dda3eee6ec94
Audrey SOBGOU <audrey.sobgouzebaze@gmail.com>
2025-01-07 22:41:23 +0100

    Create project

---
+++ Momentos/MomentosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentosApp.swift
+//  Momentos
+//
+//  Created by Audrey SOBGOU on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MomentosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MomentosTests/MomentosTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomentosTests.swift
+//  MomentosTests
+//
+//  Created by Audrey SOBGOU on 07/01/2025.
+//
+
+import Testing
+@testable import Momentos
+
+struct MomentosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
